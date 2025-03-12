e7c72b8f8c9fb22d46c43f766bcc9cd107f9a4cf
Ahmed Hossam <101903015+ahmedhoss22@users.noreply.github.com>
2025-03-12 21:48:06 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { YouTubeModule } from './youtube/youtube.module';
+//import { YouTubeModule } from './youtube/youtube.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
